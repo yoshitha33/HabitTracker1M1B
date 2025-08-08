9dc638abb6825183331fb1cbd184497d86bfdee9
yoshitha33 <yoshithanunna77@gmail.com>
2025-08-08 17:26:03 +0530

    hi

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className="text-3xl bg-amber-200 font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
